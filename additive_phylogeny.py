0901a6d38f0bad69613796f432a2b6f7f91b2b8a
ruizhang84 <rzhang360@gatech.edu>
2019-03-06 09:46:10 -0500

    Additive Phylogeny

--- additive_phylogeny.py
+++ additive_phylogeny.py
@@ -1,143 +1,139 @@
-import heapq
-import itertools
-
-class TXT_Extract:
-    def __init__(self,dna_file):
-        f = open(dna_file,'r')
-        self.lists = []
-        for line in f:
+import numpy as np
+def process_file(filename):
+    """
+    data format:
+    4
+    0   13  21  22
+    13  0   12  13
+    21  12  0   13
+    22  13  13  0
+    """
+    with open(filename) as f:
+        for i, line in enumerate(f):
             line = line.strip()
-            line = line.split(" ")
-            for n,x in enumerate(line):
-                line[n] = int(x)
-            
-            self.lists.append(line)
-    
-        self.edge = {}        #key (1,2) => length
-        self.pair = {}        #connection 1=>2
-        self.node = set()     #node
-
-        self.limb_pair ={}    # n => i, j
-        self.limb_group = {}  # distance for n => n, i,j pairs
-        
-        self.num_node=int(self.lists.pop(0)[0])
-        f.close()
-
-    def search(self,node_1,node_2):
-        """given node_1,node_2,dynamically search for the path"""
-        index = {}
-        search_path = []
-        node_next = node_1
-        
-        for node in self.node:
-            index[node] = 0
-        
-        while node_next!=node_2:  #depth first search
-            if index[node_next] < len(self.pair[node_next]):
-                index_n = index[node_next]
-                index[node_next] += 1
-                if self.pair[node_next][index_n] not in search_path:
-                    search_path += [node_next]
-                    node_next = self.pair[node_next][index_n]
+            if i == 0:
+                dim = int(line)
+                matrix = np.zeros((dim, dim), dtype=np.int32)
             else:
-                node_next = search_path.pop()
-        
-        search_path += [node_2]
-        return search_path
+                rowData = np.array(line.split()).astype(np.int32)
+                matrix[i-1] += rowData
+    return (dim, matrix)
 
-    def cal_length(self,cal_node,node_list):
-        """given a node,calculate limb length(node) and return i,j"""
-        dis = []
-        n = cal_node
-        limb_legnth = 0
-        
-        for (i,j) in itertools.combinations(node_list,2):
-            distance_ni = self.lists[n][i]
-            distance_nj = self.lists[n][j]
-            distance_ij = self.lists[i][j]
-                    
-            limb = (distance_ni+distance_nj-distance_ij)/2
-            heapq.heappush(dis,(limb,n,i,j))
+def limbLength(matrix, j):
+    """
+    Compute the Limb Length by
+    limb = min_ik(d_ij + d_jk - d_ik)/2
+    """
+    limb = np.inf
+    mask = [i for i in range(matrix.shape[0]) if i != j]
+    for k in range(matrix.shape[0]):
+        if k == j:
+            continue
+        limb = min(limb, min(matrix[mask, j]-matrix[mask,k]+matrix[j,k]))
+    return limb//2
 
-        (limb_length,n,i,j) = heapq.heappop(dis)
+def find(matrix):
+    """
+    find i, k s.t. Di,k = Di,n + Dn,k
+    """
+    for k in range(matrix.shape[0]-1):
+        arr = matrix[k] - matrix[-1]
+        index = np.where(arr == matrix[k, -1])
+        if len(index[0]) > 0:
+            return (index[0][0], k)
+    return None
 
-        self.limb_pair[n] = (i,j)
-        distance_bald_i = self.lists[n][i]-limb_length
-        distance_bald_j = self.lists[n][j]-limb_length
-        
-        return (i,j,limb_length, distance_bald_i,distance_bald_j)
-
-    def sub_tree(self):
-        """decrease the distance matrix to 2-D
-            only keep 1st 2nd column/row    """
-        node_list=[ x for x in range(self.num_node) ]
-
-        while len(node_list) >2 :
-            node = node_list.pop(1)
-            
-            (i,j,limb, distance_bald_i,distance_bald_j) = self.cal_length(node,node_list)
-            self.limb_group[(node,i)] = distance_bald_i
-            self.limb_group[(node,j)] = distance_bald_j
-            self.limb_group[(node,node)] = limb
-
-        node_distance = self.lists[ node_list[0] ][ node_list[1]  ]
-        return node_list, node_distance
-
-    def attach_limb(self):
-        """attach node back to the tree"""
-        (node_list,distance) = self.sub_tree()  #initilize
-        self.node.update(node_list)
- 
-        node_1,node_2=node_list                 #1st edge
-        self.pair[node_1] = [node_2]
-        self.pair[node_2] = [node_1]
-        self.edge[(node_1,node_2)] = distance
-        self.edge[(node_2,node_1)] = self.edge[(node_1,node_2)]
-
-        for n in range(self.num_node-2):
-            node_in  = self.num_node-2-n             #exist node inserted
-            node_new = self.num_node+n               #new   node inserted
-            
-            i,j = self.limb_pair[node_in]
-            path = self.search(i,j)                 #i -> j
-            distance_bald_i = self.limb_group[(node_in,i)]
-            distance_bald_j = self.limb_group[(node_in,j)]
-
-            distance_sum=0
-            for k in range(len(path)):
-                node_pre = path[k]
-                node_pass = path[k+1]
-
-                distance_sum += self.edge[(node_pre,node_pass)]
-                if distance_bald_i < distance_sum:
-                    self.pair[node_new] = [node_pre]
-                    self.pair[node_new] += [node_pass]
-                    self.pair[node_pre] += [node_new]
-                    self.pair[node_pre].remove(node_pass)
-                    self.pair[node_pass] += [node_new]
-                    self.pair[node_pass].remove(node_pre)
-                    self.edge[(node_pass,node_new)] = distance_sum-distance_bald_i
-                    self.edge[(node_new,node_pass)] = self.edge[(node_pass,node_new)]
-                    self.edge[(node_pre,node_new)] = self.edge[(node_pre,node_pass)]-self.edge[(node_new,node_pass)]
-                    self.edge[(node_new,node_pre)] = self.edge[(node_pre,node_new)]
-                    del self.edge[(node_pre,node_pass)]
-                    del self.edge[(node_pass,node_pre)]
-                    break
+def nearest(edge, weight, x, i, k):
+    """
+    find the nearest two nodes on path i->k
+    to insert new node, BFS
+    """
+    queue = [[i]]
+    visited = set([i])
+    findPath = []
+    while len(queue) > 0:
+        path = queue.pop()
+        node = path[-1]
+        visited.add(node)
+        if node == k:
+            findPath = path
+            break
+        for next_node in edge[node]:
+            if next_node not in visited:
+                queue.append(path+[next_node])
     
-            self.pair[node_in]  = [node_new]
-            self.pair[node_new] += [node_in]
-            self.edge[(node_in,node_new)] = self.limb_group[(node_in,node_in)]
-            self.edge[(node_new,node_in)] = self.edge[(node_in,node_new)]
-            self.node.update([node_in,node_new])
+    # distance
+    dist = 0
+    for k in range(len(findPath)-1):
+        i, j = findPath[k], findPath[k+1]
+        if dist+weight[(i, j)] > x:
+            return (i, j, x-dist, dist+weight[(i, j)]-x)
+        dist += weight[(i, j)]
+    
+    
 
-    def print_tree(self):
-        for i in self.node:
-            for j in self.pair[i]:
-                print "%d->%d:%d" %(i,j,self.edge[(i,j)])
+def additivePhylogeny(matrix, n, inner_n):
+    """
+    finds the simple tree fitting an n x n 
+    additive distance matrix D. 
+    Tree: Edge(u, v) as [... u-th[v, ...]..]
+    Weight(u, v) as {(u, v): w}
+    inner_n, count the index of inner node
+    """
+    if n == 2:
+        edge = {}
+        edge[0] = [1]
+        edge[1] = [0]
+        weight = {}
+        weight[(0, 1)] = matrix[0, 1]
+        weight[(1, 0)] = matrix[0, 1]
+        return (edge, weight, inner_n)
 
-if __name__=="__main__":
-    test=TXT_Extract('dataset_10330_6.txt')
-    test.attach_limb()
-    test.print_tree()
+    limb = limbLength(matrix, n-1)
+    matrix[:-1,-1] -= limb
+    matrix[-1,:-1] -= limb
+    #i, n, k three leaves such that Di,k = Di,n + Dn,k
+    i, k = find(matrix)
+    x = matrix[i, -1]
+    # remove row n and column n from D
+    edge, weight, inner_n = additivePhylogeny(matrix[:-1, :-1], n-1, inner_n)
+    # the (potentially new) node in T at distance x from i on the path between i and k
+    
+     # find the insert node
+    i_near, k_near, i_x, n_x = nearest(edge, weight, x, i, k)
+    new_node = i_near
+
+    # check if need to creat new node
+    if i_x != 0:
+        new_node = inner_n
+        inner_n += 1
+        # insert between i, k
+        edge[i_near].remove(k_near)
+        edge[k_near].remove(i_near)
+        edge[i_near].append(new_node)
+        edge[k_near].append(new_node)
+        edge[new_node] = [i_near, k_near]
+
+        weight[(new_node, i_near)] = i_x
+        weight[(i_near, new_node)] = i_x
+        weight[(new_node, k_near)] = n_x
+        weight[(k_near, new_node)] = n_x
+        del weight[(i_near, k_near)]
+        del weight[(k_near, i_near)]
+    # add leaf n back to T by creating a limb (v, n) of length limbLength
+    edge[new_node].append(n-1)
+    edge[n-1] = [new_node]
+    weight[(n-1, new_node)] = limb
+    weight[(new_node, n-1)] = limb
+    return (edge, weight, inner_n)
 
 
+def printOut(edge, weight):
+    for i in sorted(edge):
+        for j in sorted(edge[i]):
+            print (str(i)+"->"+str(j)+":"+str(weight[(i, j)]))
+
+
+n, matrix = process_file("rosalind_ba7c.txt")
+edge, weight, _ = additivePhylogeny(matrix, n, n)
+printOut(edge, weight)
