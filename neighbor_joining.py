56713b77f785dea041f32bc673a7e91dcd2b76a1
ruizhang84 <rzhang360@gatech.edu>
2019-03-06 09:47:42 -0500

    Neighbor-Joining Algorithm

--- neighbor_joining.py
+++ neighbor_joining.py
@@ -1,142 +1,98 @@
-class TXT_Extract:
-    def __init__(self,dna_file):
-        f = open(dna_file,'r')
-        
-        self.lists = []
-        for line in f:
+def process_file(filename):
+    """
+    data format:
+    4
+    0   13  21  22
+    13  0   12  13
+    21  12  0   13
+    22  13  13  0
+    """
+    with open(filename) as f:
+        for i, line in enumerate(f):
             line = line.strip()
-            line = line.split("\t")
-            for n,x in enumerate(line):
-                line[n] = float(x)
-            self.lists.append(line)
-        
-        self.tree = {}  # node1 <-> node2 => length
-        self.root = {} # node <= node1, node2
-        self.leaf = {}# node => node1, node2
-        
-        self.num_node = int(self.lists.pop(0)[0])
-        
-        f.close()
+            if i == 0:
+                dim = int(line)
+                matrix = {}
+            else:
+                row = line.split()
+                matrix[i-1] = {}
+                for j in range(len(row)):
+                    matrix[i-1][j] = float(row[j])
+    return (dim, matrix)
 
-    def build_tree(self):
-        interpret = { x: x for x in range(len(self.lists)) }
-        search_i = 0
-        search_j = 1
-        
-        for n in range(len(self.lists)-3) :
-            (search_i, search_j) = search_min(self.lists)# neighbor search
-            (limb_1,limb_2) = self.nj_tree(search_i, search_j)
-
-            self.root[interpret[search_i]] = self.num_node+n
-            self.root[interpret[search_j]] = self.num_node+n
-            self.leaf[self.num_node+n] = (interpret[search_i],interpret[search_j])
-            self.tree[interpret[search_i]] = limb_1
-            self.tree[interpret[search_j]] = limb_2
-        
-            interpret[search_i] = self.num_node+n   # update list order
-            temp_dic = interpret.copy()
-            for m in range(search_j,len(self.lists)):
-                interpret[m] = temp_dic[m+1]
-
-        search_i,search_j = [x for x in range(len(self.lists)) if x != search_i] ##the other side node
-        (limb_1,limb_2) = self.nj_tree(search_i, search_j)
-        self.root[interpret[search_i]] = self.num_node*2-3
-        self.root[interpret[search_j]] = self.num_node*2-3
-        self.leaf[self.num_node*2-3] = (interpret[search_i],interpret[search_j])
-        self.tree[interpret[search_i]] = limb_1
-        self.tree[interpret[search_j]] = limb_2
-        
-        self.root[self.num_node*2-3] = self.num_node*2-4 #node1 <==> node2
-        self.root[self.num_node*2-4] = self.num_node*2-3
-        self.tree[self.num_node*2-3] = self.lists[0][1]
-        self.tree[self.num_node*2-4] = self.lists[0][1]
-                
-    
-    def print_tree(self):
-        
-        for i in range(self.num_node*2-2):
-            if i in self.leaf:
-                print "%d->%d:%.3f" %(i,self.leaf[i][0],self.tree[self.leaf[i][0]])
-                print "%d->%d:%.3f" %(i,self.leaf[i][1],self.tree[self.leaf[i][1]])
-            if i in self.root:
-                print "%d->%d:%.3f" %(i, self.root[i],self.tree[i])
-    
-    
-
-    def nj_tree(self,search_i,search_j):
-        """D_k,m = (1/2)(D_k,i + D_k,j - Di,j)"""
-        new_lists = []
-        lists_range = len(self.lists)
-        for i in range(lists_range):
-            row_lists = []
-            for j in range(lists_range):
-                if i == search_i :
-                    if j == search_i:
-                        row_lists.append(0)
-                    elif j == search_j:
-                        continue
-                    else:
-                        d_km = (self.lists[search_i][j] + self.lists[search_j][j] - self.lists[search_i][search_j])/2
-                        row_lists.append(d_km)
-                elif i == search_j:
+def neighborJoining(matrix, n):
+    if n == 2:
+        edge = {}
+        weight = {}
+        for i in matrix:
+            for j in matrix[i]:
+                if  j == i:
                     continue
-                elif j == search_i:
-                    d_km = (self.lists[i][search_i] + self.lists[i][search_j] - self.lists[search_i][search_j])/2
-                    row_lists.append(d_km)
-                elif j == search_j:
-                    continue
-                else:
-                    row_lists.append(self.lists[i][j])
-            if len(row_lists) != 0:
-                new_lists.append(row_lists)
-        
-        total_dist = total_distance(self.lists)
-        d_ij = (total_dist[search_i] - total_dist[search_j])/(float(lists_range) - 2.0)
-        limb_1 = (self.lists[search_i][search_j] + d_ij)/2.0
-        limb_2 = (self.lists[search_i][search_j] - d_ij)/2.0
-        
-        self.lists = [ row[:] for row in new_lists ]
-        return limb_1, limb_2
+                edge[i] = [j]
+                edge[j] = [i]
+                weight[(i, j)] = matrix[i][j]
+                weight[(j, i)] = matrix[i][j]
+        return (edge, weight)
+    # Construct neighbor-joining matrix D* from D.
+    totalDistance = {}
+    for i in matrix:
+        totalDistance[i] = 0
+        for j in matrix[i]:
+            totalDistance[i] += matrix[i][j]
+    MatrixD = {}
+    for i in matrix:
+        MatrixD[i] = {}
+        for j in matrix[i]:
+            if i == j:
+                MatrixD[i][j] = 0
+            else:
+                MatrixD[i][j] = (n-2)*matrix[i][j] - totalDistance[i] - totalDistance[j]
+    # Find a minimum element D*i,j of D*.
+    mini = float("inf")
+    _i = _j = None
+    for i in MatrixD:
+        for j in MatrixD[i]:
+            if MatrixD[i][j] < mini:
+                mini = MatrixD[i][j]
+                _i, _j = i, j
+    # Compute delta(i,j) = (TotalDistanceD(i) –TotalDistanceD(j)) / (n – 2).
+    delta = (totalDistance[_i] - totalDistance[_j])/(n-2.0)
+    # Set LimbLength(i) equal to 1/2(delta_i,j + delta_i,j) and LimbLength(j) equal to 1/2(Di,j – delta_i,j).
+    limbLength_i = (matrix[_i][_j] + delta)/2.0
+    limbLength_j = (matrix[_i][_j] - delta)/2.0
+    # Form a matrix D' by removing i-th and j-th row/column from D and adding an m-th row/column 
+    # such that for any k, Dk,m = (Di,k + Dj,k – Di,j) / 2.
+    rows = list(matrix)
+    m = max(matrix) + 1
+    matrix[m] = {}
+    matrix[m][m] = 0.0
+    for k in rows:
+        if k != _i and k != _j:
+            matrix[m][k] = (matrix[_i][k] + matrix[_j][k] - matrix[_i][_j])/2.0
+            matrix[k][m] = matrix[m][k]
+            del matrix[k][_i]
+            del matrix[k][_j]
+    del matrix[_i]
+    del matrix[_j]
+    # Apply NeighborJoining to D' to obtain Tree(D').
+    edge, weight = neighborJoining(matrix, n-1)
+    # Reattach limbs of i and j to obtain Tree(D).
+    edge[_i] = [m]
+    edge[m].append(_i)
+    edge[_j] = [m]
+    edge[m].append(_j)
+    weight[(_i, m)] = limbLength_i
+    weight[(m, _i)] = limbLength_i
+    weight[(_j, m)] = limbLength_j
+    weight[(m, _j)] = limbLength_j
+    return edge, weight
 
 
-def total_distance(lists):
-    total_dist = [ float(sum(lists[x])) for x in range(len(lists)) ]
-    
-    return total_dist
-
-def search_min(lists):
-    search_i = 0
-    search_j = 1
-    
-    lists_t = nj_matrix(lists) ##neighbor-joining matrix
-    
-    small = lists_t[0][1]
-    for i in range(len(lists_t)):
-        for j in range(i+1, len(lists_t)):
-            if lists_t[i][j] < small:
-                search_i = i
-                search_j = j
-                small = lists_t[i][j]
-
-    return (search_i,search_j)
-
-def nj_matrix(lists):
-    """d_ij =(n-2)*d_ij-total_i-total_j"""
-    nj_len = len(lists)
-    nj_lists = [ row[:] for row in lists ]
-
-    total_dist = total_distance(lists)
-    for i in range(nj_len):
-        for j in range(nj_len):
-            if i != j:
-                nj_lists[i][j] = (nj_len-2)*lists[i][j]-total_dist[i]-total_dist[j]
-
-    return nj_lists
 
 
-if __name__=="__main__":
-    test=TXT_Extract('dataset_10333_6.txt')
-    test.build_tree()
-    test.print_tree()
-
-
+n, matrix = process_file("rosalind_ba7e.txt")
+edge, weiight = neighborJoining(matrix, n)
+for i in sorted(edge):
+    for j in edge[i]:
+        w = "%.3f" %weiight[(i, j)]
+        print (str(i)+"->"+str(j)+":"+w)
